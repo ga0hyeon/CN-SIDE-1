32aafe71a2558f3cb522099875c1ec645acb781e
Jasper <jongho77@lgcns.com>
2023-04-07 13:15:05 +0900

    zustand 적용된 ui-components의 카운터 가져와서 적용

--- packages/chromeside-board/src/App.tsx
+++ packages/chromeside-board/src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useCounterStore } from "ui-components";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { count, countUp, resetCount } = useCounterStore((state: any) => {
+    return {
+      count: state.count,
+      countUp: state.countUp,
+      resetCount: state.resetCount,
+    };
+  });
 
   return (
     <div className="App">
@@ -16,20 +23,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + zustand</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => countUp()}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        <button onClick={() => resetCount()}>reset</button>
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
