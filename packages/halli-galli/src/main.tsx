e031845ca09984a79f75f8ea011d30700284462f
Cinnamon <jeeyeon.park@lgcns.com>
2023-04-20 13:22:28 +0900

    add main page and router

--- packages/halli-galli/src/main.tsx
+++ packages/halli-galli/src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import EnterPage from './page/EnterPage'
+import GamePage from './page/GamePage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<EnterPage />}></Route>
+        <Route path={"/game"} element={<GamePage />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
--- packages/ui-components/index.ts
+++ packages/ui-components/index.ts
@@ -1,4 +1,5 @@
 import useCountStore from "./src/store/useCountStore";
 import { Button } from "./src/components/Button";
+import { EnterButton } from "./src/components/EnterButton";
 
-export { useCountStore, Button };
+export { useCountStore, Button, EnterButton };
