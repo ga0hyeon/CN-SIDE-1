866b67d6c064379f0c152a6e7ecca070cef15117
Jasper <jongho77@lgcns.com>
2023-04-25 15:29:18 +0900

    styled component를 이용하여 pixel button 개발

--- packages/ui-components/src/components/PixelButton.tsx
+++ packages/ui-components/src/components/PixelButton.tsx
@@ -1,36 +1,84 @@
 import React from "react";
-import "./pixelButton.css";
+import styled from "styled-components";
+import { darkerRgb } from "../utils/ColorUtil";
 
 interface PixelButtonProps {
   primary?: boolean;
   backgroundColor?: string;
-  size?: "small" | "medium" | "large";
   label: string;
   onClick?: () => void;
 }
+const Container = styled.div`
+  top: 50%;
+  left: 50%;
+  position: absolute;
+  transform: translate(-50%, -50%);
+`;
+
+const PixelizeButton = styled.button<{ backgroundColor?: string }>`
+  font-size: 1.2em;
+  font-family: "Neo Dgm", cursive;
+  padding: 0 20px;
+  height: 70px;
+  background: ${({ backgroundColor }) => backgroundColor || "#06c1de"};
+  border: 0px;
+  position: relative;
+  box-shadow: inset -4px 2px 1px 1px grey, inset -4px -2px 1px 1px lightgray,
+    inset 4px 0px 1px 1px lightgray;
+
+  :hover {
+    cursor: pointer;
+    background: ${({ backgroundColor }) =>
+      darkerRgb(backgroundColor, 0.1) || "#06b7d1"};
+  }
+
+  :active {
+    top: 5px;
+    box-shadow: -4px 2px 1px 1px grey, -4px -2px 1px 1px lightgray,
+      4px 0px 1px 1px lightgray;
+  }
+
+  ::after {
+    content: "";
+    background: black;
+    position: absolute;
+    left: -2.5%;
+    top: 0;
+    width: 105%;
+    height: 100%;
+    z-index: -1;
+  }
+
+  ::before {
+    content: "";
+    background: black;
+    position: absolute;
+    left: 0;
+    top: -5%;
+    width: 100%;
+    height: 113%;
+    z-index: -1;
+  }
+`;
 
 export const PixelButton = ({
-  primary = false,
-  size = "medium",
   backgroundColor,
   label,
   onClick,
   ...props
 }: PixelButtonProps) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
+  console.log(darkerRgb(backgroundColor || "", 0.1));
   return (
-    <div className="container">
-      <button
+    <Container>
+      <PixelizeButton
         type="button"
         className={["btn"].join(" ")}
-        style={{ backgroundColor }}
+        backgroundColor={backgroundColor}
         {...props}
         onClick={() => onClick && onClick()}
       >
         {label}
-      </button>
-    </div>
+      </PixelizeButton>
+    </Container>
   );
 };
