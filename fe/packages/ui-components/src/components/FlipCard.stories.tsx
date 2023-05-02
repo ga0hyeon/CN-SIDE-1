23cfb80693ca60c3336f8f3c4cb8b561d9dca9fd
Jasper(이종호 선임) <jongho77@lgcns.com>
2023-05-02 21:59:32 +0900

    FlipCard 제작

---
+++ fe/packages/ui-components/src/components/FlipCard.stories.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { FlipCard } from "./FlipCard";
+
+export default {
+  title: "Example/FlipCard",
+  component: FlipCard,
+
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof FlipCard>;
+
+const Template: ComponentStory<typeof FlipCard> = (args) => (
+  <FlipCard {...args} />
+);
+
+export const Basic = Template.bind({});
+Basic.args = {
+  front: <h1>앞면</h1>,
+  titleColor: "#000",
+  frontColor: "tomato",
+  back: <h1>뒷면</h1>,
+  backColor: "royalblue",
+};
+
+export const Circle = Template.bind({});
+Circle.args = {
+  front: <h1>앞면</h1>,
+  frontColor: "tomato",
+  back: <h1>뒷면</h1>,
+  backColor: "royalblue",
+  type: "circle",
+};
