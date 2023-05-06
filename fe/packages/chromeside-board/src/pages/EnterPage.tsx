5463adf176a681a6b7acdd527af77bae7c915c47
ga0hyeon <idoll0513@gmail.com>
2023-05-06 20:00:33 +0200

    page 폴더를 pages 폴더로 바꾸어 depth를 맞춤

--- fe/packages/chromeside-board/src/pages/EnterPage.tsx
+++ fe/packages/chromeside-board/src/pages/EnterPage.tsx
@@ -1,7 +1,25 @@
-const EnterPage = () => {
-    return <div>
-        EnterPage
-    </div>
+import { useState } from 'react'
+import '../App.css'
+import { EnterButton as EnterButton } from "ui-components";
+import { useNavigate } from "react-router-dom";
+
+function EnterPage() {
+    const movePage = useNavigate();
+
+    return (
+        <div className='App'>
+            <span>Awesome Logo or Name</span>
+            <div>
+                <input placeholder='Nickname Here'></input>
+            </div>
+            <div>
+                <EnterButton label='' backgroundColor='grey'
+                    onClick={() => {
+                        movePage('/game')
+                    }}></EnterButton>
+            </div>
+        </div>
+    )
 }
 
 export default EnterPage;
--- fe/packages/chromeside-board/src/pages/GamePage.tsx
+++ fe/packages/chromeside-board/src/pages/GamePage.tsx
@@ -1,7 +1,15 @@
-const GamePage = () => {
-    return <div>
-        GamePage
-    </div>
+import { useState } from 'react'
+import '../App.css'
+import { EnterButton as EnterButton } from "ui-components";
+import { useNavigate } from "react-router-dom";
+
+function GamePage() {
+
+    return (
+        <div className='App'>
+            <span>Game Page</span>
+        </div>
+    )
 }
 
 export default GamePage;
