d770efbc35d99e639371519cfb47d5cb5216d3f2
cherry <yunju715.choi@gmail.com>
2023-05-07 16:05:48 +0900

    chrome extension에서 BrowserRouter 동작하지 않아 HashRouter로 변경

--- fe/packages/chromeside-board/src/main.tsx
+++ fe/packages/chromeside-board/src/main.tsx
@@ -5,17 +5,17 @@ import './index.css'
 import './tailwind.css'
 import EnterPage from './page/EnterPage'
 import GamePage from './page/GamePage'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 console.log(import.meta.env.VITE_WSS_URL)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path={"/"} element={<EnterPage />}></Route>
         <Route path={"/game"} element={<GamePage />}></Route>
         <Route path={"/test"} element={<App />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
