5e9e8b688341ee6e9bc116877ecd17c7a40dab5d
cherry <yunju715.choi@gmail.com>
2023-05-07 17:53:04 +0900

    tailwind.css 파일 중복 제거

--- fe/packages/halli-galli/src/main.tsx
+++ fe/packages/halli-galli/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './tailwind.css'
+import './../../../tailwind.css'
 import './index.css'
 import EnterPage from './page/EnterPage'
 import GamePage from './page/GamePage'
