e25a98eb9d3a8fd0e6c7759ebcfde27d22c6d736
Jasper(이종호 선임) <jongho77@lgcns.com>
2023-05-08 21:57:30 +0900

    할리갈리 로직

--- fe/packages/halli-galli/src/HalliGalli.tsx
+++ fe/packages/halli-galli/src/HalliGalli.tsx
@@ -1,13 +1,6 @@
 import { useRef, useState } from "react";
 // import "../App.css";
 
-enum CardType {
-  Strawberry,
-  Cherry,
-  Banana,
-  Plum,
-}
-
 interface Card {
   type: string;
   num: number;
@@ -20,19 +13,17 @@ interface PlayerCard {
   player: Player;
   cards: Card[];
 }
-interface OpenCard {
-  player: Player;
-  type: string;
-  num: number;
-}
 
 const CARD_COUNT = [1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5];
+const CARD_TYPE = ["Strawberry", "Banana", "Cherry", "Plum"];
 const CARD: Card[] = [];
 
 const HalliGalli = () => {
+  const [nickname, setNickname] = useState("");
   const [playerCards, setPlayerCards] = useState<PlayerCard[]>([]);
   const [players, setPlayers] = useState<Player[]>([]);
   const [openedCard, setOpendCard] = useState<Card[][]>([[], [], [], []]);
+  const [playerState, setPlayerState] = useState<boolean[]>([true, true, true, true]);
   const [activePlayer, setActivePlayer] = useState(0);
   const turn = useRef(0);
 
@@ -52,68 +43,122 @@ const HalliGalli = () => {
 
     return arr;
   };
+  
+    
 
   const handleJoin = () => {
-    const temp = [];
-    temp.push({
-      nickname: "a",
-      index: 0,
-    });
-    temp.push({
-      nickname: "b",
-      index: 1,
-    });
-    temp.push({
-      nickname: "c",
-      index: 2,
-    });
-    temp.push({
-      nickname: "d",
-      index: 3,
-    });
+    if(players.length < 4) {
+      console.log("JOIN ", nickname);
+      // const temp = [...players];
+      setPlayers( prev => [...prev, {
+          nickname: nickname,
+          index: prev.length
+        }]
+      )
+  
+      setNickname("");
+    } else {
+      console.log("ROOM is Full");
+    }
+    
+  };
 
-    setPlayers(temp);
-
+  const handleStart = () => {
     CARD_COUNT.forEach((k, i) => {
-      for (let j = 0; j < temp.length; j++) {
+      for (let j = 0; j < players.length; j++) {
         CARD.push({
-          type: CardType[j],
+          type: CARD_TYPE[j],
           num: k,
         });
       }
     });
-  };
 
-  const handleStart = () => {
     const shuffledCards = cardShuffle(cardShuffle(CARD));
 
     setPlayerCards(cardSplitter(shuffledCards, players, 14));
     setActivePlayer(turn.current % 4);
+
+    console.log("START");
   };
 
   const handleOpen = () => {
+    console.log(players[activePlayer].nickname + "'s card open");
+    console.log("OPEN", playerCards);
+
     const pc = playerCards[activePlayer];
-    const card = pc.cards.splice(0, 1)[0];
 
-    setOpendCard((prev) => {
-      const newCardList = [...prev];
-      newCardList[activePlayer] = [...newCardList[activePlayer], card];
-      return newCardList;
-    });
+    if(pc.cards.length > 0) {
+      const card = pc.cards.splice(0, 1)[0];
 
-    setPlayerCards((prev) => {
-      prev[activePlayer] = pc;
-      return prev;
-    });
+      setOpendCard((prev) => {
+        const newCardList = [...prev];
+        newCardList[activePlayer] = [...newCardList[activePlayer], card];
+        return newCardList;
+      });
+  
+      setPlayerCards((prev) => {
+        prev[activePlayer] = pc;
+        return prev;
+      });
+    } else {
+      setPlayerState(prev => {
+        prev[activePlayer] = false;
+        return prev;
+      })
+    }
+    
     turn.current++;
 
     setActivePlayer(turn.current % 4);
   };
 
+  const handleBell = (playerIndex:number) => {
+    console.log(players[playerIndex].nickname + " touch bell");
+
+    // emit ring
+    const sum = [0,0,0,0];
+    
+    openedCard.map((cards, index) => {
+      let lastCard:Card = cards[cards.length-1];
+      
+      if(lastCard) {
+        sum[CARD_TYPE.findIndex(x=> x===lastCard.type)] += lastCard.num;
+      }
+
+    });
+
+    if(sum.filter(x => x ===5).length > 0) {
+      // Success
+      let award:Card[] = [];
+      
+      openedCard.map((cards, i) => {
+        award.push(...cards);
+      });
+
+      const copy = [...playerCards];
+      copy[playerIndex].cards.push(...award);
+      
+      setPlayerCards(copy);
+
+      setOpendCard([[],[],[],[]]);
+    } else {
+      // Panalty
+      const copy = [...playerCards];
+
+      copy.map((pc, i) => {
+        if(i != playerIndex && playerState[playerIndex] ) {
+          pc.cards.push(...copy[playerIndex].cards.splice(0,1));
+        }
+      })
+      setPlayerCards(copy);
+
+      console.log("Panalty");
+    }
+  }
+
   return (
     <div className="App">
       <div>turn : {turn.current}</div>
-      <div>player : {players.length}</div>
       <div>cards : {playerCards.length}</div>
       <div>
         activePlayer: {players.length > 0 ? players[activePlayer].nickname : ""}
@@ -122,13 +167,21 @@ const HalliGalli = () => {
       {players.map((p, i) => {
         return (
           <div key={i}>
-            <div>player : {p.nickname}</div>
+            <div>player : [{p.index}, {playerState[p.index]?"alive" :"lose"}]{p.nickname}</div>
+          </div>
+        );
+      })}
+      <br></br>
+      {playerCards.map((p, i) => {
+        return (
+          <div key={i}>
+            <div>player : [{p!.player!.nickname}]{p!.cards!.length}</div>
           </div>
         );
       })}
       <br></br>
       <div>
-        Open Cards
+        Opened Cards
         {openedCard.map((c, i) => {
           return (
             <div key={i}>
@@ -145,11 +198,17 @@ const HalliGalli = () => {
         })}
       </div>
       <br></br>
-      <button onClick={() => handleJoin()}>join</button>
-      <button onClick={() => handleStart()}>start</button>
-      <button onClick={() => handleOpen()}>open</button>
-      <button>bell</button>
-      <button>ring</button>
+      <label>nickname: </label><input onChange={(e)=> {
+        setNickname(e.target.value);
+      }} value={nickname}/>
+      <button onClick={() => handleJoin()}>join</button><br/>
+      <button onClick={() => handleStart()}>start</button><br/>
+      <button onClick={() => handleOpen()}>open</button><br/>
+      <button onClick={() => handleBell(0)}>bell0</button>
+      <button onClick={() => handleBell(1)}>bell1</button>
+      <button onClick={() => handleBell(2)}>bell2</button>
+      <button onClick={() => handleBell(3)}>bell3</button><br/>
+      <button>ring</button><br/>
       <button
         onClick={() => {
           console.log(players);
