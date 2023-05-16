744e2331dd0edec9aafaeeae967c68e26fef6948
cherry <yunju.choi@lgcns.com>
2023-05-16 17:54:33 +0900

    프로필 이미지 추가

--- fe/packages/ui-components/src/components/UserSlot.tsx
+++ fe/packages/ui-components/src/components/UserSlot.tsx
@@ -10,12 +10,12 @@ import PurpleCardImage from "../assets/images/purple4.png";
 import GreenCardImage from "../assets/images/green3.png";
 import CardBackImage from "../assets/images/card_back_2.png";
 import ProfileImage from "../assets/images/profile.png";
+import ProfileImage2 from "../assets/images/profile2.png";
 interface UserSlotProps {
-    position: "LEFT_TOP" | "LEFT_BOTTOM" | "RIGHT_TOP" | "RIGHT_BOTTOM";
-    user: User;
-    onClick?: () => void;
+  position: "LEFT_TOP" | "LEFT_BOTTOM" | "RIGHT_TOP" | "RIGHT_BOTTOM";
+  user: User;
+  onClick?: () => void;
 }
-
 
 const Container = styled.div<{}>`
   width: 45%;
@@ -25,26 +25,27 @@ const Container = styled.div<{}>`
 `;
 
 const UserInfo = styled.div<{}>`
-  min-width : 180px;
+  min-width: 180px;
   justify-content: center;
   align-items: center;
   margin-top: 40px;
-  margin-left:auto;
+  margin-left: auto;
 
   .user-name {
     font-size: 1.2rem;
     font-weight: bold;
-    margin-left : 30px;
+    margin-left: 30px;
     font-family: "Neo Dgm", cursive;
     background-color: rgb(90 142 106);
-    width : 120px;
+    width: 120px;
     justify-content: center;
     align-items: center;
   }
 
   .user-profile {
-    margin : auto;
+    margin: auto;
     height: 80px;
+    border-radius: 50%;
   }
 `;
 
@@ -53,59 +54,80 @@ const CardArea = styled.div<{}>`
   width: 100px;
 `;
 
-
 export const UserSlot = ({
-    position,
-    user,
-    onClick,
-    ...props
+  position,
+  user,
+  onClick,
+  ...props
 }: UserSlotProps) => {
+  const getCardImage = () => {
+    if (position === "LEFT_TOP") return BlueCardImage;
+    if (position === "LEFT_BOTTOM") return RedCardImage;
+    if (position === "RIGHT_TOP") return GreenCardImage;
+    if (position === "RIGHT_BOTTOM") return PurpleCardImage;
+  };
 
-    const getCardImage = () => {
-        if(position === "LEFT_TOP")  return BlueCardImage;
-        if(position === "LEFT_BOTTOM")  return RedCardImage;
-        if(position === "RIGHT_TOP")  return GreenCardImage;
-        if(position === "RIGHT_BOTTOM")  return PurpleCardImage;
-    }
+  const getProfileImage = () => {
+    if (position === "RIGHT_TOP")
+      return (
+        <img
+          className="user-profile"
+          src={ProfileImage2}
+          width={85}
+          height={120}
+        />
+      );
     return (
-        <Container>
-            {(position == "LEFT_TOP" || position == "LEFT_BOTTOM")
-                && <>
-                    <UserInfo>
-                        {user &&
-                            <>
-                                <img className="user-profile" src={user.profileImage} />
-                                <div className="user-name">{user.userName}</div>
-                            </>
-                        }
-                    </UserInfo>
-                    <CardArea>
-                        <img src={CardBackImage} width='105'/>
-                    </CardArea>
-                </>}
-            <CardArea>
-                <FlipCard type={'circle'}
-                    front={
-                        <img src={CardBackImage} width='105'/>}
-                    frontColor={"000000ff"}
-                    back={
-                        <img src={getCardImage()} width='105'/>}
-                        // <img src={require("../assets/images/blue1.png")}/>}
-                    backColor={"000000ff"} />
-            </CardArea>
-            {(position == "RIGHT_TOP" || position == "RIGHT_BOTTOM")
-                && <>
-                    <CardArea>
-                        <img src={CardBackImage} width='105'/>
-                    </CardArea>
-                    <UserInfo>
-                    {user &&
-                        <>
-                            <img className="user-profile" src={user.profileImage} />
-                            <div className="user-name">{user.userName}</div>
-                        </>
-                    }
-                </UserInfo></>}
-        </Container>
+      <img
+        className="user-profile"
+        src={ProfileImage}
+        width={80}
+        height={120}
+      />
     );
+  };
+  return (
+    <Container>
+      {(position == "LEFT_TOP" || position == "LEFT_BOTTOM") && (
+        <>
+          <UserInfo>
+            {user && (
+              <>
+                {getProfileImage()}
+                <div className="user-name">{user.userName}</div>
+              </>
+            )}
+          </UserInfo>
+          <CardArea>
+            <img src={CardBackImage} width="105" />
+          </CardArea>
+        </>
+      )}
+      <CardArea>
+        <FlipCard
+          type={"circle"}
+          front={<img src={CardBackImage} width="105" />}
+          frontColor={"000000ff"}
+          back={<img src={getCardImage()} width="105" />}
+          // <img src={require("../assets/images/blue1.png")}/>}
+          backColor={"000000ff"}
+        />
+      </CardArea>
+      {(position == "RIGHT_TOP" || position == "RIGHT_BOTTOM") && (
+        <>
+          <CardArea>
+            <img src={CardBackImage} width="105" />
+          </CardArea>
+          <UserInfo>
+            {user && (
+              <>
+                {getProfileImage()}
+                <div className="user-name">{user.userName}</div>
+              </>
+            )}
+          </UserInfo>
+        </>
+      )}
+    </Container>
+  );
 };
